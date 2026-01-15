7b2b45269e642d8957de9c5eff21ba58c7fff85d
Riya Shukla <missriyashukla@gmail.com>
2026-01-15 14:34:39 +0530

    feat:added Size validation

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -2,6 +2,7 @@ package com.ecommerce.project.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,7 +15,12 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
-    @NotBlank private String categoryName;
+
+    @NotBlank
+
+//    @Size(min = 5) default
+    @Size(min = 5,message = "Category name must have 5 character")
+    private String categoryName;
 
 //    @Version
 //    private Long version;
