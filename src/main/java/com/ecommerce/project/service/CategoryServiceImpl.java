afea56b5048a386e2c96d7854bdac19de66787b9
Riya Shukla <missriyashukla@gmail.com>
2026-01-25 21:29:36 +0530

    feat:added Product module

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -19,7 +19,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class CategoryServiceimpl implements CategoryService {
+public class CategoryServiceImpl implements CategoryService {
 
 
 
