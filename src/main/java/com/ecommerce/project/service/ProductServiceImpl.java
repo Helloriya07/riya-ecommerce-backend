a4e91af20a8d079229cfc4283c53cf9fd6eb2179
Riya Shukla <missriyashukla@gmail.com>
2026-01-26 22:16:39 +0530

    feat: added endpoint getProductByKeyword

--- src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
@@ -18,7 +18,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-public class ProductServiceImpl implements ProductService{
+public class ProductServiceImpl implements ProductService {
 
     @Autowired
     private ProductRepository productRepository;
@@ -30,11 +30,11 @@ public class ProductServiceImpl implements ProductService{
     @Override
     public ProductDTO addProduct(Long categoryId, Product product) {
         Category category = categoryRepository.findById(categoryId)
-                .orElseThrow(()-> new ResponseNotFoundException("Category","categoryId",categoryId));
-       product.setImage("default.png");
+                .orElseThrow(() -> new ResponseNotFoundException("Category", "categoryId", categoryId));
+        product.setImage("default.png");
         product.setCategory(category);
         double specialPrice = product.getPrice()
-                -((product.getDiscount()*0.01) *product.getPrice());
+                - ((product.getDiscount() * 0.01) * product.getPrice());
         product.setSpecialPrice(specialPrice);
         Product savedProduct = productRepository.save(product);
         return modelMapper.map(savedProduct, ProductDTO.class);
@@ -42,11 +42,11 @@ public class ProductServiceImpl implements ProductService{
 
     @Override
     public ProductResponse getAllProducts() {
-        List<Product> products= productRepository.findAll();
+        List<Product> products = productRepository.findAll();
         List<ProductDTO> productDTOS = products.stream()
                 .map(product -> modelMapper.map(product, ProductDTO.class))
                 .collect(Collectors.toList());
-        ProductResponse productResponse =  new ProductResponse();
+        ProductResponse productResponse = new ProductResponse();
         productResponse.setContent(productDTOS);
         return productResponse;
     }
@@ -54,15 +54,27 @@ public class ProductServiceImpl implements ProductService{
     @Override
     public ProductResponse searchByCategory(Long categoryId) {
         Category category = categoryRepository.findById(categoryId)
-                .orElseThrow(()->
-                        new ResponseNotFoundException("Category","categoryId",categoryId));
+                .orElseThrow(() ->
+                        new ResponseNotFoundException("Category", "categoryId", categoryId));
         List<Product> products = productRepository.findByCategoryOrderByPriceAsc(category);
         List<ProductDTO> productDTOS = products.stream()
                 .map(product -> modelMapper.map(product, ProductDTO.class))
                 .collect(Collectors.toList());
-        ProductResponse productResponse =  new ProductResponse();
+        ProductResponse productResponse = new ProductResponse();
         productResponse.setContent(productDTOS);
         return productResponse;
     }
 
+    @Override
+    public ProductResponse searchProductByKeyword(String keyword) {
+
+        List<Product> products = productRepository.findByProductNameLikeIgnoreCase('%' + keyword + '%');
+        List<ProductDTO> productDTOS = products.stream()
+                .map(product -> modelMapper.map(product, ProductDTO.class))
+                .collect(Collectors.toList());
+        ProductResponse productResponse = new ProductResponse();
+        productResponse.setContent(productDTOS);
+        return productResponse;
+
+    }
 }
