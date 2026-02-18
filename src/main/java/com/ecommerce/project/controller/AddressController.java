ff76c021e3fad3ab532ef033ccdf928c1c077854
Riya Shukla <missriyashukla@gmail.com>
2026-02-18 20:41:50 +0530

    feat: getting address update

--- src/main/java/com/ecommerce/project/controller/AddressController.java
+++ src/main/java/com/ecommerce/project/controller/AddressController.java
@@ -40,4 +40,11 @@ public class AddressController {
         AddressDTO addressDTO = addressService.getAddressesById(addressId);
         return  new ResponseEntity<>(addressDTO, HttpStatus.OK);
     }
+    @GetMapping("/users/addresses")
+    public ResponseEntity<List<AddressDTO>> getUserAddress()
+    {
+        User user = authUtil.loggedInUser();
+        List<AddressDTO> addressList = addressService.getUserAddresses(user);
+        return  new ResponseEntity<>(addressList, HttpStatus.OK);
+    }
 }
