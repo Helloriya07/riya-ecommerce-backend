7d0edd522cc861bafdd55d3ad591365b311b0317
Riya Shukla <missriyashukla@gmail.com>
2026-02-18 20:08:45 +0530

    feat: getting address

--- src/main/java/com/ecommerce/project/service/AddressServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/AddressServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ecommerce.project.service;
 
+import com.ecommerce.project.exception.ResponseNotFoundException;
 import com.ecommerce.project.model.Address;
 import com.ecommerce.project.model.User;
 import com.ecommerce.project.payload.AddressDTO;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AddressServiceImpl implements AddressService {
@@ -29,4 +31,26 @@ public class AddressServiceImpl implements AddressService {
         Address savedAddress = addressRepository.save(address);
         return  modelMapper.map(savedAddress, AddressDTO.class);
     }
+
+    @Override
+    public List<AddressDTO> getAddresses() {
+        List<Address> addresses = addressRepository.findAll();
+       List<AddressDTO> addressDTOs =  addresses.stream()
+                .map(address -> modelMapper.map(address, AddressDTO.class))
+                .collect(Collectors.toList());
+        return addressDTOs;
+    }
+
+    @Override
+    public AddressDTO getAddressesById(Long addressId) {
+        Address address = addressRepository.findById(addressId)
+                .orElseThrow(()-> new ResponseNotFoundException("Address","addressId",+addressId));
+        return modelMapper.map(address, AddressDTO.class);
+    }
+
+
+
+
+
+
 }
